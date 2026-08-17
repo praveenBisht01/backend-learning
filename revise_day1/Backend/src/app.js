8bd2e013d6497180fbfd17ea02534310e1c781e4
priyanshu <priyanshubist95@gmail.com>
2026-08-17 23:07:19 +0530

    added frontend

--- revise_day1/Backend/src/app.js
+++ revise_day1/Backend/src/app.js
@@ -1,9 +1,10 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 app.use(express.json());
-const Note_model = require("./model/notemodel")
+const Note_model = require("./model/notemodel");
 
+// post data on database   //
 app.post("/api/notes", async (req, res) => {
   const { title, description } = req.body;
 
@@ -13,11 +14,45 @@ app.post("/api/notes", async (req, res) => {
   });
 
   res.status(201).json({
-    message: "Note created successfully",
+    Message: "note created successfully",
     note,
   });
 });
 
+// get data from Database //
+
+app.get("/api/notes", async (req, res) => {
+  const note = await Note_model.find();
+
+  res.status(200).json({
+    Message: "note Fetched successfully",
+    note,
+  });
+});
+
+// delete noted by ID   //
+
+app.delete("/api/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const note_delete = await Note_model.findByIdAndDelete(id);
+
+  res.status(200).json({
+    Message: "note delete successfully",
+    note_delete,
+  });
+});
+
+//  Update the note //
+
+app.patch("/api/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const { description } = req.body;
+  note_update = await Note_model.findByIdAndUpdate(id, { description });
+
+  res.status(200).json({
+    Message: "note update successfully",
+    note_update,
+  });
+});
+
 module.exports = app;
-
-
---
+++ revise_day1/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [notes, setNotes] = useState("");
+  return (
+    <div>
+      hye 
+    </div>
+  )
+}
+
+export default App
