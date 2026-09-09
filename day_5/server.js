286d66204f15217b3a8fe5d98afb9c77bfdc4c8c
Praveen Singh <priyanshubist95@gmail.com>
2026-09-09 20:51:44 +0530

    Clean up server.js and update console log
    Removed unnecessary blank lines and fixed console log message formatting.

--- day_5/server.js
+++ day_5/server.js
@@ -5,14 +5,9 @@ const dns = require("dns");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const Cnt_to_DB = require("./config/Database");
 
-
-
-
-
-
 Cnt_to_DB();
 
 app.listen(3000,()=> {
-    console.log("this server is running on port number 3000")
+    console.log("this server is running on port  3000")
 })
 
